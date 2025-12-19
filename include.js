3dad20fa64e445bb44d46b9d29f90ae73b59be5e
wesleyfletcher <wesley.fletcher@icloud.com>
2025-12-19 00:38:53 -0500

    Setup templates, added navbar styles

--- include.js
+++ include.js
@@ -1,3 +1,7 @@
-fetch("/navbar.html")
+fetch("/templates/head.html")
+    .then(r => r.text())
+    .then(html => document.getElementsByTagName("head")[0].innerHTML += html)
+
+fetch("/templates/navbar.html")
     .then(r => r.text())
     .then(html => document.getElementsByTagName("nav")[0].innerHTML += html)
